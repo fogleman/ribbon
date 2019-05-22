065cb6ca75257e83dc357f89041a23a583ad36a7
Michael Fogleman <fogleman@gmail.com>
2019-05-21 22:01:42 -0400

    Add RemoveChain function

--- pdb/model.go
+++ pdb/model.go
@@ -11,3 +11,37 @@ type Model struct {
 	Residues    []*Residue
 	Chains      []*Chain
 }
+
+func (model *Model) RemoveChain(chainID string) {
+	atoms := model.Atoms[:0]
+	for _, atom := range model.Atoms {
+		if atom.ChainID != chainID {
+			atoms = append(atoms, atom)
+		}
+	}
+	model.Atoms = atoms
+
+	hetAtoms := model.HetAtoms[:0]
+	for _, atom := range model.HetAtoms {
+		if atom.ChainID != chainID {
+			hetAtoms = append(hetAtoms, atom)
+		}
+	}
+	model.HetAtoms = hetAtoms
+
+	residues := model.Residues[:0]
+	for _, residue := range model.Residues {
+		if residue.ChainID != chainID {
+			residues = append(residues, residue)
+		}
+	}
+	model.Residues = residues
+
+	chains := model.Chains[:0]
+	for _, chain := range model.Chains {
+		if chain.ChainID != chainID {
+			chains = append(chains, chain)
+		}
+	}
+	model.Chains = chains
+}
